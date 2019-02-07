2f2fca3e34515c2a8eea3d10f5be927bfe109bd7
Thierry Tournie <thierrytournie@comhem.com>
2019-02-07 15:10:24 +0100

    adding simple habit check again

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, ScrollView, View, Button, TouchableOpacity } from 'react-native';
-import { SwipeListView } from 'react-native-swipe-list-view';
+import { StyleSheet, Text, ScrollView, View, Button, TouchableOpacity, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { removeHabit, checkHabit, unCheckHabit } from '../../reducers/habits';
 import { isHabitDoneToday } from '../../utils/dates';
@@ -9,93 +8,93 @@ import { isHabitDoneToday } from '../../utils/dates';
 class HomeScreen extends React.Component {
     state = {
         editMode: false,
-      };
+    };
 
     static navigationOptions = ({ navigation }) => {
         return {
-          headerTitle: 'My Habits',
-          headerRight: (
-            <TouchableOpacity
-                style={styles.addButton}
-                onPress={() => navigation.navigate('AddHabit')}
-            >
-                <Ionicons name="md-add" color="#43c744" size={25} />
-            </TouchableOpacity>
-          ),
-          headerLeft: (
-            <Button
-              onPress={navigation.getParam('switchEditMode')}
-              title={navigation.getParam('EditTile') || 'Edit'}
-              color="#43c744"
-            />
-          ),
+            headerTitle: 'My Habits',
+            headerRight: (
+                <TouchableOpacity
+                    style={styles.addButton}
+                    onPress={() => navigation.navigate('AddHabit')}
+                >
+                    <Ionicons name="md-add" color="#43c744" size={25} />
+                </TouchableOpacity>
+            ),
+            headerLeft: (
+                <Button
+                    onPress={navigation.getParam('switchEditMode')}
+                    title={navigation.getParam('EditTile') || 'Edit'}
+                    color="#43c744"
+                />
+            ),
         };
-      };
+    };
 
     componentDidMount() {
         this.props.navigation.setParams({ switchEditMode: this.toggleEditMode });
     }
-    
+
     toggleEditMode = () => {
         this.setState({ editMode: !this.state.editMode });
         this.props.navigation.setParams({ EditTile: this.state.editMode ? 'Edit' : 'Done' });
-      };
+    };
 
     deleteHabit = (item) => {
         this.props.removeHabit(item);
-      };
+    };
 
     renderHabitItem = (item) => (
         <React.Fragment>
-            {this.state.editMode && <TouchableOpacity style={styles.deleteButton} onPress={() => this.deleteHabit(item)}>
-                <Text style={styles.deleteButtonText}>delete</Text>
-            </TouchableOpacity>}
             <Text>{item.name}</Text>
+            {isHabitDoneToday(item) && <View style={styles.doneIcon}><Ionicons color='#43c744' name="md-checkmark-circle" size={25} /></View>}
         </React.Fragment>
     )
-    
-    onSwipeValueChange = (swipeData) => {
-        const { key, value } = swipeData;
-        if (value > 80) {
-            const habit = this.props.habitList.find(item => item.key === key);
-            isHabitDoneToday(habit) ? this.props.unCheckHabit(habit) : this.props.checkHabit(habit);
-        }
+
+    renderHabitItemWrapper = (item) => (
+        <React.Fragment>
+            {!this.state.editMode &&
+                <TouchableOpacity style={styles.toggleHabit} onPress={() => this.toggleHabit(item)}>
+                    <View style={styles.habitItemContainer}>
+                        {this.renderHabitItem(item)}
+                    </View>
+                </TouchableOpacity>
+            }
+            {this.state.editMode &&
+                <View style={styles.habitItemContainer}>
+                    <TouchableOpacity style={styles.deleteButton} onPress={() => this.deleteHabit(item)}>
+                        <Ionicons color='red' name="md-remove-circle" size={25} />
+                    </TouchableOpacity>
+                    {this.renderHabitItem(item)}
+                </View>
+            }
+        </React.Fragment>
+    )
+
+    toggleHabit = (item) => {
+        isHabitDoneToday(item) ? this.props.unCheckHabit(item) : this.props.checkHabit(item);
     }
 
     render() {
         return (
-        <ScrollView contentContainerStyle={styles.container}>
-            {this.props.habitList.length === 0 && <Text>Start adding habits</Text>}
-            <SwipeListView
-                useFlatList
-                closeOnScroll
-                style={styles.habitContainer}
-                data={this.props.habitList}
-                extraData={this.state}
-                renderItem={({item}) => (
-                    <View style={styles.habitItemContainer}>
-                        {this.renderHabitItem(item)}
-                        {isHabitDoneToday(item) && <View style={styles.doneIcon}><Ionicons color='#43c744' name="md-checkmark-circle" size={25} /></View>}
-                    </View>
-                )
-            }
-            renderHiddenItem={(data) => (
-                <View style={styles.rowBack}>
-                    <View style={styles.doneButton}><Text>Left</Text></View>
-                    <Text>Right</Text>
-                </View>
-                )
-            }
-            leftOpenValue={375}
-            rightOpenValue={-75}
-            onSwipeValueChange={this.onSwipeValueChange}
-            stopLeftSwipe={120}
-            />
-        </ScrollView>
+            <ScrollView contentContainerStyle={styles.container}>
+                {this.props.habitList.length === 0 && <Text>Start adding habits</Text>}
+                <FlatList
+                    useFlatList
+                    closeOnScroll
+                    style={styles.habitContainer}
+                    data={this.props.habitList}
+                    extraData={this.state}
+                    renderItem={({ item }) => (
+                        this.renderHabitItemWrapper(item)
+                    )
+                    }
+                />
+            </ScrollView>
         );
     }
 }
-  
+
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#fff",
@@ -104,7 +103,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     habitItemContainer: {
-        flex:1,
+        flex: 1,
         flexDirection: "row",
         alignItems: "center",
         height: 60,
@@ -113,34 +112,26 @@ const styles = StyleSheet.create({
         paddingLeft: 10,
         backgroundColor: "#fff",
         borderColor: "#CED0CE",
+        marginBottom: 5,
+    },
+    toggleHabit: {
+        backgroundColor: "#fff",
     },
     deleteButton: {
         marginRight: 20,
     },
     deleteButtonText: {
-        color:"#43c744"
+        color: "#43c744"
     },
     addButton: {
         marginRight: 10,
     },
     doneIcon: {
-        flex:1,
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-end',
         marginRight: 10,
     },
-    rowBack: {
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        backgroundColor: "#43c744",
-        alignItems: 'center',
-        borderColor: "#CED0CE",
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: 15,
-        paddingRight: 15,
-      },
 });
 
 const mapStateToProps = (state) => ({
@@ -149,5 +140,5 @@ const mapStateToProps = (state) => ({
 
 export default connect(
     mapStateToProps,
-    {removeHabit, checkHabit, unCheckHabit}
+    { removeHabit, checkHabit, unCheckHabit }
 )(HomeScreen);
